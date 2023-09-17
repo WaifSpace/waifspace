27e1845e8d24b2127e100e68c986a5c436ce1ff9
jimxl <tianxiaxl@gmail.com>
2023-09-17 14:42:10 +0800

    修改了chatgpt翻译的输出

--- lib/app/services/ai_service.dart
+++ lib/app/services/ai_service.dart
@@ -40,19 +40,17 @@ class AIService {
     if(text.length >= 400) {
       text = text.substring(0, 400);
     }
-    logger.i('[AI 翻译] $text');
     OpenAIChatCompletionModel chatCompletion = await OpenAI.instance.chat.create(
       model: model,
       messages: [
         OpenAIChatCompletionChoiceMessageModel(
-          // content: "请将后面文章的核心内容用中文总结出来，总字数在 $maxLength 字以内, 文章内容如下: $text",
           content: "翻译 $text 到中文",
           role: OpenAIChatMessageRole.user,
         ),
       ],
     );
     var result = chatCompletion.choices.first.message.content;
-    logger.i('[AI 翻译的结果是] $result');
+    logger.i('[AI 翻译] $text => $result');
     return result;
   }
 
