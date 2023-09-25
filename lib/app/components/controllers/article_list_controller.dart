c59a62e320c572f77853c588300c59a70db1abaf
jimxl <tianxiaxl@gmail.com>
2023-09-26 01:20:15 +0800

    发现了 listview 渲染的bug

--- lib/app/components/controllers/article_list_controller.dart
+++ lib/app/components/controllers/article_list_controller.dart
@@ -52,6 +52,7 @@ class ArticleListController extends GetxController {
   }
 
   Future<void> loadMore() async {
+    // 这里的触发机制有一个bug，如果只有一篇文章的时候，因为离列表的最后位置很近，所以会一直触发滑到底部加载更多文章的问题
     if (_isLoadMore == true) {
       return;
     }
